fb333deea93e954742c68281a9b6948cbb7859b1
Bro7Alexis <an.brossette@gmail.com>
2022-03-28 14:37:21 +0200

    Auth

--- pages/auth.jsx
+++ pages/auth.jsx
@@ -1,3 +1,15 @@
+import { useState } from "react";
+import { SvgBtn } from "../components/button";
+
 export default function AuthPage() {
-  return <div className={"bg-slate-800"}>Hello World !</div>;
+  const [mode, setMode] = useState(false);
+
+  return (
+    <div className="flex bg-stone-700 h-screen w-screen justify-center items-center">
+      <main className="flex border rounded-sm border-stone-300">
+        {/* {mode ? <Login /> : <Signup />} //!Import customComponent */}
+        <SvgBtn action={setMode} svg="true" />
+      </main>
+    </div>
+  );
 }
