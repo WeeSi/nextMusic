9843c3e6df13581e1091466d03f95f63deb77432
weesi <francklucasehui@gmail.com>
2022-03-28 12:05:21 +0200

    added fakeapi

---
+++ fakeapi/usersJson.js
@@ -0,0 +1,30 @@
+const users = [{
+    firstname:"Franck",
+    lastname:"Ehui",
+    username : "WeeSii",
+    password : "1234",
+    image : "",
+    _id:1
+},{
+    firstname:"Alexis",
+    lastname:"Brossette",
+    username : "RengeSan",
+    password : "azerty",
+    image : "",
+    _id:2
+},{
+    firstname:"Florian",
+    lastname:"Spedalotto",
+    username : "Florian",
+    password : "qsdf",
+    image : "",
+    _id:3
+},{
+    firstname:"Grégoire",
+    lastname:"",
+    username : "greg",
+    password : "eorto",
+    image : "",
+    _id:4
+},
+]
