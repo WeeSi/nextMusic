3fb2802507e3a0f8efc68941532cf0b61d339c0b
Bro7Alexis <an.brossette@gmail.com>
2022-03-28 16:53:39 +0200

    Signup base

--- components/input.jsx
+++ components/input.jsx
@@ -1,14 +1,37 @@
 export default function MnInput({
   type = "text",
   w = "full",
-  reg,
+  label = "label",
+  name,
+  register,
+  required = false,
+  pattern = "",
+  error,
   placeholder,
+  autocomplete = "email",
 }) {
   return (
-    <input
-      type={type}
-      className={`flex grow w-${w} outline-hidden focus:outline-none p-2`}
-      placeholder={placeholder}
-    />
+    <div className="relative w-full">
+      <label className="text-stone-300">
+        {label}
+        {required && "*"}
+      </label>
+      <input
+        type={type}
+        autoComplete={autocomplete}
+        id={label}
+        placeholder={placeholder}
+        className={`flex grow w-${w} outline-hidden focus:outline-none p-2`}
+        {...register(name, {
+          required: required,
+          pattern: pattern,
+        })}
+      />
+      {error && (
+        <span style={{ color: "#ff4d6f" }} className="error text-sm">
+          {error.message}
+        </span>
+      )}
+    </div>
   );
 }
