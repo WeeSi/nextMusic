e2c24c18a16130e28381ee45f35af174e0491651
DESKTOP-JGLHB1P\Gregoire <gregoire.raturat@modaal.fr>
2022-03-30 17:23:41 +0200

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import AuthPage from '../components/auth/auth';
-import HomePlayer
- from './home';
+import HomePlayer from './home';
 import { Context } from '../context';
 import { useContext, useEffect } from 'react';
-import HomePlayer from './home';
 
 function Index(props) {
   const {state,dispatch} = useContext(Context);
