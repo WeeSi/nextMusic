e5a5f58c7ce4070a1343c4495f3658e186f246ae
DESKTOP-JGLHB1P\Gregoire <gregoire.raturat@modaal.fr>
2022-03-30 11:32:27 +0200

    api spotify

--- components/cardSearch.jsx
+++ components/cardSearch.jsx
@@ -1,22 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { CircularProgress, Link } from '@mui/material';
 
-export default function CardSearch({data}) {
-    console.log("test1",data)
+export default function CardSearch({data,loading}) {
     return (
-        <Card className='absolute w-full' style={{maxHeight: '50vh', overflow: 'auto', display:''}}>
-            {data.map((item, index) => (
+        <Card className='absolute w-full' style={{maxHeight: '50vh', overflow: loading||'auto', display:''}}>
+            {loading && 
+                <div className='flex justify-center p-2'><CircularProgress size={50}/></div>
+                    
+            }
+            {data && data.map((item, index) => (
             <Box sx={{ display: 'block', flexDirection: 'row' }} key={index}>
+                <Link href={item.data.albumOfTrack.sharingInfo.shareUrl} target="_blank" style={{ textDecoration: 'none' }}> 
                 <CardContent sx={{ flex: '1 0 auto' }}>
-                    <Typography component="div" variant="h5">
-                        {item.data.name}
-                    </Typography>
-                    <Typography variant="subtitle1" color="text.secondary" component="div">
-                        {item.data.artists.items[0].profile.name}
-                    </Typography>
+                    <div className='flex'>
+                        <img src={item.data.albumOfTrack.coverArt.sources[0].url}style={{maxHeight: 90, maxWidth: 90}} className='p-2' alt={item.data.albumOfTrack.coverArt.sources[0].url}/>
+                        <Typography component="div" variant="h6">
+                            {item.data.name}
+                            <Typography variant="subtitle1" color="text.secondary" component="div">
+                                {item.data.artists.items[0].profile.name}
+                            </Typography>
+                        </Typography>
+                    </div>
                 </CardContent>
+                </Link> 
             </Box>
             ))}
         </Card>
--- pages/home.js
+++ pages/home.js
@@ -24,29 +24,29 @@ function HomePlayer(props) {
                 <thead className="">
                   <tr>
                     <th scope="col" className="relative py-3 pl-4 pr-3 sm:pr-6">
-                      <span className="text-gray-500">#</span>
+                      <span>#</span>
                     </th>
                     <th
                       scope="col"
-                      className="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500 sm:pl-6"
+                      className="py-3 pl-4 pr-3 text-left text-xs font-medium uppercase tracking-wide  sm:pl-6"
                     >
                       Title
                     </th>
                     <th
                       scope="col"
-                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide "
                     >
                       Artist
                     </th>
                     <th
                       scope="col"
-                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide "
                     >
                       Time
                     </th>
                     <th
                       scope="col"
-                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide text-gray-500"
+                      className="px-3 py-3 text-left text-xs font-medium uppercase tracking-wide "
                     >
                       Album
                     </th>
@@ -56,7 +56,7 @@ function HomePlayer(props) {
                 <tbody >
                   {Songs.map((song,index) => (
                     <tr className="song-list-table" key={song.id}>
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-center m-auto text-sm font-medium text-gray-900 ">
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-center m-auto text-sm font-medium">
                         <>
                           <PlayIcon
                             onClick={() => dispatch({type:"CHANGE_SONG", payload:song})}
@@ -65,12 +65,12 @@ function HomePlayer(props) {
                           <span className="play-song-incr-number">{index+1}</span>
                         </>
                       </td>
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 ">
+                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium">
                         {song.artist}
                       </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{song.title}</td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{song.time}</td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{song.album}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm ">{song.title}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm ">{song.time}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm ">{song.album}</td>
                     </tr>
                   ))}
                 </tbody>
